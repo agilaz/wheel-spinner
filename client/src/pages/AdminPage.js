e7a3a183fad4d576c858b4cd6bfac0589915956b
agilaz <erk700@gmail.com>
2020-12-27 23:42:57 -0500

    Update admin page, pull out spin/angle control to parent instead of wheel

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,21 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import API, { getErrorMessage } from '../api';
 import { Wheel } from '../components/Wheel';
+import { Button } from 'react-bootstrap';
+import randomSpin from '../util/randomSpin';
 
 const AdminPage = ({match}) => {
+    const [spin, setSpin] = useState(null);
+    const [rotation, setRotation] = useState(0);
     const [error, setError] = useState(null);
     const [wedges, setWedges] = useState(null);
     const [winner, setWinner] = useState(null);
 
-    const handleWinner = (winnerIndex) => {
-        setWinner(wedges[winnerIndex]);
-        const newWedges = [...wedges];
-        newWedges[winnerIndex] = {...newWedges[winnerIndex], hidden: false};
-        setWedges(newWedges);
+    function updateWheel(newWedges) {
         API.updateWheel(match.params.id, match.params.hash, {wedges: newWedges})
             .then(resp => resp.data)
             .then(data => setWedges(data.wedges))
             .catch((err) => setError(getErrorMessage(err)));
+    }
+
+    const handleSpinEnd = (winnerIndex, rotation) => {
+        setSpin(null);
+        setRotation(rotation);
+        setWinner(wedges[winnerIndex]);
+        const newWedges = [...wedges];
+        newWedges[winnerIndex] = {...newWedges[winnerIndex], hidden: false};
+        setWedges(newWedges);
+        updateWheel(newWedges);
+    };
+
+    const hideAllWedges = () => {
+        const newWedges = [...wedges].map(wedge => ({...wedge, hidden: true}));
+        updateWheel(newWedges);
     };
 
     useEffect(() => {
@@ -54,7 +69,19 @@ const AdminPage = ({match}) => {
             <p>{winner && winner.label}</p>
             Wheel:
             <br />
-            <Wheel size={500} wedges={wedges} onWinner={handleWinner} />
+            <Wheel size={500} wedges={wedges} onSpinEnd={handleSpinEnd} spin={spin} initialRotation={rotation}/>
+            <Button
+                variant={'primary'}
+                disabled={!!spin}
+                onClick={() => setSpin(randomSpin)}>
+                Spin
+            </Button>
+            <Button
+                variant={'warning'}
+                disabled={!!spin}
+                onClick={hideAllWedges}>
+                Hide All
+            </Button>
         </div>
     );
 }
--- client/src/util/index.js
+++ client/src/util/index.js
@@ -1,2 +1,3 @@
 export { default as randomColor } from './randomColor';
 export { default as useIsMount } from './useIsMount';
+export { default as randomSpin } from './randomSpin';
